91462752174893e253ff57fb2b9543106fde05e8
PROPIETARIO-PC\Propietario <sergiolozanobueno@gmail.com>
2018-02-15 15:32:37 +0100

    Creacion de test divisible3

--- tests/FizzBuzzGameTest.php
+++ tests/FizzBuzzGameTest.php
@@ -9,7 +9,15 @@ include_once 'src/FizzBuzzGame.php';
 final class FizzBuzzGameTest extends TestCase
 {
 
-}
+  public function testDivisble3()
+  {
+    $calc = new FizzBuzzGameClass();
+    $this->assertEquals(
+        "Fizz",
+        $calc->FizzBuzzGamecalc(3)
+      );
+    }
+  }
 
 
  ?>
