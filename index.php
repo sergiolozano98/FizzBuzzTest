06152df880c47b4491593aaccddf1f9ec75c284d
PROPIETARIO-PC\Propietario <sergiolozanobueno@gmail.com>
2018-02-15 16:50:03 +0100

    nueva funcion FizzBuzzGameArray

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
-$number=1;
-for ($number = $number; $number <= 100; $number++)
-{
-
-  if($number % 3 == 0 && $number % 5 != 0 ){
-    echo "Fizz <br>";
-  }
-
-  else if($number % 5 == 0 && $number % 3 != 0 ){
-    echo "Buzz <br>";
-  }
-  else if($number % 3 == 0 && $number % 5 ==0){
-    echo "FizzBuzz <br>";
-  }
-  else {
-    echo "$number <br>";
-  }
-}
+include 'src/FizzBuzzGame.php';
+$list = new FizzBuzzGameClass();
+$list->FizzBuzzGameArray();
+var_dump($list);
  ?>
--- src/FizzBuzzGame.php
+++ src/FizzBuzzGame.php
@@ -7,13 +7,14 @@ class FizzBuzzGameClass
 {
   function __construct()
   {
+    echo "test";
   }
+  private $init=1;
+  private $end=100;
   /*Creamos los funcion que nos calculara si es divisible entre 5,3 o entre los 2*/
   /*La variable $number sera el numero que comprobamos*/
   public function FizzBuzzGamecalc($number)
   {
-    for ($number = $number; $number <= 100; $number++)
-    {
       /*debemos poner que tambien que no sea divisible por 5 para que en el test no devuelva 2 valores por que si no habra conflicto con divisibles entre 3 y 5*/
       if($number % 3 == 0 && $number % 5 != 0 ){
         return "Fizz";
@@ -28,7 +29,13 @@ class FizzBuzzGameClass
       else {
         return $number;
       }
-    }
+  }
+  public function FizzBuzzGameArray()
+  {
+      for ($number=$this->init; $number <= $this->end ; $number++) {
+        $lista[] = $this->FizzBuzzGamecalc($number);
+      }
+      return $lista;
   }
 }
 
