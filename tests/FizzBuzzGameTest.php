78d65319e9adeaf78b7587bf83db5a19b16222be
PROPIETARIO-PC\Propietario <sergiolozanobueno@gmail.com>
2018-02-15 15:34:52 +0100

    creacion de test divisible5

--- tests/FizzBuzzGameTest.php
+++ tests/FizzBuzzGameTest.php
@@ -17,7 +17,15 @@ final class FizzBuzzGameTest extends TestCase
         $calc->FizzBuzzGamecalc(3)
       );
     }
-  }
 
+  public function testDivisble5()
+  {
+    $calc = new FizzBuzzGameClass();
+    $this->assertEquals(
+      "Buzz",
+      $calc->FizzBuzzGamecalc(5)
+    );
+  }
+}
 
  ?>
